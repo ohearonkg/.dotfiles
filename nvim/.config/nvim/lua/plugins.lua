3f1662342a1820a907de3c3a6cdfe1e2bb927460
KG O'Hearon <ohearonkg@gmail.com>
2021-12-17 16:48:00 -0400

    fix: add missing comma within the plugins file for nvim-tree

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -33,7 +33,7 @@ return require('packer').startup({function(use)
     'kyazdani42/nvim-tree.lua',
     requires = {
       'kyazdani42/nvim-web-devicons', 
-    }
+    },
     config = get_config('nvim-tree')
   }
 
